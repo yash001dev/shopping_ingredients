cfd2acd446d366c725455be9fb0e38c268e525f1
yash001dev <y.sanghavi001@gmail.com>
2020-06-21 15:49:30 +0530

    Basic Projection Added

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://cdn.pixabay.com/photo/2014/10/22/16/38/ingredients-498199_960_720.jpg",
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('French Fries',20),
+      ],
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is simply a test",
+      "https://cdn.pixabay.com/photo/2015/09/05/01/05/french-fries-923687_960_720.jpg",
+      [
+        new Ingredient('Buns',2),
+        new Ingredient('Meat',1),
+      ],
+    ),
+  ];
+  constructor(private slService:ShoppingListService){};
+  getRecipe() {
+    return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients:Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+}
